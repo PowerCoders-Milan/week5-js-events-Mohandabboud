b70f0a9edfcd0829db966924df0b544f5e4b6da7
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-04 11:16:35 +0100

    solving ex1

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,50 @@
 // define your functions here
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+function changeColor () {
+    a[0].style.color = getRandomColor();
+    a[1].style.color = getRandomColor();
+    a[2].style.color = getRandomColor();
+  }
+  function changeColour () {
+    a[0].style.color = "black"
+    a[1].style.color = "black"
+    a[2].style.color = "black"
+}
 
 // call the functions and connect them to an event
+
+var a = document.querySelectorAll("strong");
+var b = document.querySelector("a")
+console.log(a);
+b.addEventListener("mouseenter", changeColor);
+b.addEventListener("mouseleave",changeColour);
+
+
+
+/* function changeColor () {
+    a[0].style.color = "red"
+    a[1].style.color = "red"
+    a[2].style.color = "red"
+  }
+  function changeColour () {
+    a[0].style.color = "black"
+    a[1].style.color = "black"
+    a[2].style.color = "black"
+}
+
+// call the functions and connect them to an event
+
+var a = document.querySelectorAll("strong");
+var b = document.querySelector("a")
+console.log(a);
+b.addEventListener("mouseenter", changeColor);
+b.addEventListener("mouseleave",changeColour);
+*/
