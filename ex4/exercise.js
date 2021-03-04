aa3a8b518f268d29ba7ef38b24bfcb579cb1b455
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-04 14:43:43 +0100

    solving ex4 without extra

---
+++ ex4/exercise.js
@@ -0,0 +1,28 @@
+
+/*
+function myFunction() {
+    var x = document.getElementById("typehere").value;
+    var p = document.querySelector("p")
+    p.innerHTML= x;
+    console.log(p);
+    
+  } 
+  */
+ 
+
+
+/*
+  function submit() {
+    var x = document.getElementById("typehere").value;
+    let p1 = document.createElement("p"); 
+    p1.innerHTML = x;
+    var body = document.querySelector("body");
+    body.appendChild(p1);
+  } */
+
+  function myFunction() {
+    var x = document.getElementById("typehere").value;
+    let p = document.querySelector("p");
+    p.innerHTML= x;
+    console.log(p);
+}
